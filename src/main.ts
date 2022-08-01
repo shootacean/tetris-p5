c489cf36c47b24921f52c73403f771c195a0a819
Shota Narisaka <shootacean@gmail.com>
2022-08-02 08:37:37 +0900

    feat(*): 方向キーでもミノを移動できるようにする

--- src/main.ts
+++ src/main.ts
@@ -243,20 +243,35 @@ const s = (p: p5) => {
   }
   let game: Game;
   
-  p.keyPressed = function keyPressed() {
+  p.keyPressed = function keyPressed(): void {
+    // Left
     if (p.keyCode === 65) game.minoVx = -1;
+    if (p.keyCode === 37) game.minoVx = -1;
+
+    // Right
     if (p.keyCode === 68) game.minoVx = 1;
+    if (p.keyCode === 39) game.minoVx = 1;
+
+    // Up
+    // if (p.keyCode === 37);
+
+    // Down
+    // if (p.keyCode === 40);
+
+    // Rotate Left
     if (p.keyCode === 81) game.minoVr = -1;
+
+    // Rotate Right
     if (p.keyCode === 69) game.minoVr = 1;
   }
   
-  p.setup = function setup() {
+  p.setup = function setup(): void {
     p.createCanvas(400, 600);
     p.background(64);
     game = new Game();
   }
   
-  p.draw = function draw() {
+  p.draw = function draw(): void {
     game.proc();
   }
   
